379409727e16549328b11be05e744dbadb9e92f1
Enzxco <lawrencesecjadas@gmail.com>
2025-02-27 15:47:14 +0800

    Update Act_2

--- activity2.php
+++ activity2.php
@@ -4,9 +4,7 @@ require_once("vendor/autoload.php");
 
 $faker = Faker\Factory::create('en_PH');
 
-$genres = ['Fiction', 'Mystery', 'Science Fiction', 'Fantasy', 'Romance', 'Thriller', 'Historical', 'Horror'];
-
-echo "<table border='1'spacing= '2' padding='2'>";
+echo "<table border='1' cellspacing='2' cellpadding='2'>";
 echo "<tr>
         <th>Title</th>
         <th>Author</th>
@@ -20,13 +18,12 @@ for ($i = 0; $i <= 15; $i++) {
     echo "<tr>";
         echo "<td>" . $faker->words(3, true) . "</td>"; 
         echo "<td>" . $faker->name . "</td>";
-        echo "<td>" . $genres[array_rand($genres)] . "</td>";
-        echo "<td>" . $faker->numberBetween(1900,2024) . "</td>";
+        echo "<td>" . $faker->randomElement(['Fiction', 'Mystery', 'Science Fiction', 'Fantasy', 'Romance', 'Thriller', 'Historical', 'Horror']) . "</td>"; 
+        echo "<td>" . $faker->numberBetween(1900, 2024) . "</td>";
         echo "<td>" . $faker->isbn13 . "</td>";
         echo "<td>" . $faker->paragraph . "</td>";
     echo "</tr>";
 }
 
 echo "</table>";
-
 ?>
